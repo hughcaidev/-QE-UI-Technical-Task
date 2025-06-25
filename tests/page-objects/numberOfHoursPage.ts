5fe8941aaeaf03e942c27dd468324a3f972a1502
Hugh Cai <hughcaidev@gmail.com>
2025-06-25 10:48:05 +0100

    refine page objects

--- tests/page-objects/numberOfHoursPage.ts
+++ tests/page-objects/numberOfHoursPage.ts
@@ -1,23 +1,28 @@
-import { Page } from "@playwright/test"
+import { Locator, Page } from "@playwright/test"
 
 class NumberOfHoursPage {
     readonly page: Page
+    readonly numberOfHoursField: Locator
+    readonly continueButton: Locator
+
     constructor(page) {
         this.page = page
+        this.numberOfHoursField = this.page.getByRole("textbox", {
+            name: "Number of hours worked per",
+        })
+        this.continueButton = this.page.getByRole("button", {
+            name: "Continue",
+        })
     }
 
     async enterHours(numberOfHours: string) {
-        await this.page
-            .getByRole("textbox", { name: "Number of hours worked per" })
-            .click()
-        await this.page
-            .getByRole("textbox", { name: "Number of hours worked per" })
-            .fill(numberOfHours)
+        await this.numberOfHoursField.click()
+        await this.numberOfHoursField.fill(numberOfHours)
     }
 
     async submitNumberOfHours(numberOfHours: string) {
         await this.enterHours(numberOfHours)
-        await this.page.getByRole("button", { name: "Continue" }).click()
+        await this.continueButton.click()
     }
 }
 
--- tests/page-objects/startDatePage.ts
+++ tests/page-objects/startDatePage.ts
@@ -2,21 +2,29 @@ import { Locator, Page } from "@playwright/test"
 
 class StartDatePage {
     readonly page: Page
+    readonly dayField: Locator
+    readonly monthField: Locator
+    readonly yearField: Locator
     readonly continueButton: Locator
 
     constructor(page) {
         this.page = page
-        this.continueButton = page.locator("button", { hasText: "Continue" })
+        this.dayField = this.page.getByRole("textbox", { name: "Day" })
+        this.monthField = this.page.getByRole("textbox", { name: "Month" })
+        this.yearField = this.page.getByRole("textbox", { name: "Year" })
+        this.continueButton = this.page.locator("button", {
+            hasText: "Continue",
+        })
     }
 
     async enterDate(date) {
         const [day, month, year] = date.split("/")
-        await this.page.getByRole("textbox", { name: "Day" }).click()
-        await this.page.getByRole("textbox", { name: "Day" }).fill(day)
-        await this.page.getByRole("textbox", { name: "Day" }).click()
-        await this.page.getByRole("textbox", { name: "Month" }).fill(month)
-        await this.page.getByRole("textbox", { name: "Month" }).click()
-        await this.page.getByRole("textbox", { name: "Year" }).fill(year)
+        await this.dayField.click()
+        await this.dayField.fill(day)
+        await this.monthField.click()
+        await this.monthField.fill(month)
+        await this.yearField.click()
+        await this.yearField.fill(year)
     }
 
     async submitDate(date) {
